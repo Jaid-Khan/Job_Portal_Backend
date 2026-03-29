d7180240a523da943df2a717027ecdeef272fc73
JAID-KHAN <jaidkhan04072001@gmail.com>
2026-03-30 01:23:23 +0530

    added cors universal access

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ const postRoutes = require("./routes/postRoutes");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: "*"
+}));
 app.use(express.json());
 
 app.get("/", (req, res)=>{
